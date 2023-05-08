34e10bc98a38494080ad99c75b10eacca8b5829f
Ankur Saikia <112326589+ankursaikia23@users.noreply.github.com>
2023-05-08 15:00:52 +0530

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-eyah+a-jxc$s$7lazot!m)uto%e%e^**!mzgf5a*45-x-_=89(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alexg0.pythonanywhere.com']
 
 
 # Application definition
